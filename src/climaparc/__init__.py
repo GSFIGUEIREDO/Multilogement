8b44b228b1bd2897fbb53bb424fe653e5a104a32
GSFIGUEIREDO <gfigueiredo@klimfax.ca>
2026-08-31 16:50:59 -0400

    Delete src/climaparc directory

--- src/climaparc/__init__.py
+++
@@ -1,1 +0,0 @@
-"""ClimaParc application package."""
--- src/climaparc/reports/domain/repositories.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from typing import Protocol
-
-
-class ReportsDataRepository(Protocol):
-    def get_report_data(self) -> dict:
-        ...
